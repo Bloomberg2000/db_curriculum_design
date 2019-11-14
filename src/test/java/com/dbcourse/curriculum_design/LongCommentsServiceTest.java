c37008a2a1642b324615f72f548737e2f12af3ae
christy <christy99cc@qq.com>
2019-11-14 17:10:29 +0800

    Update LongCommentsServiceTest.java
    增加通过电影查找长评的测试

--- src/test/java/com/dbcourse/curriculum_design/LongCommentsServiceTest.java
+++ src/test/java/com/dbcourse/curriculum_design/LongCommentsServiceTest.java
@@ -17,7 +17,11 @@ public class LongCommentsServiceTest {
     @Test
     void  getLongCommentsByMovieIdTest(){
         List<LongComments> longCommentsList = longCommentsService.getLongCommentsByMovieId(1);
-        if (!longCommentsList.get(0).getCTitle().equals("hhh"))
+        if (!longCommentsList.get(0).getCTitle().equals("长评题目1"))
+            throw new RuntimeException();
+        if (!longCommentsList.get(1).getCTitle().equals("长评题目2"))
+            throw new RuntimeException();
+        if (longCommentsList.size()!=2)
             throw new RuntimeException();
 
     }
