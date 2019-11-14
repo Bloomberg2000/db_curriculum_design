4c743f3f43ae6058cab91eaeec1b5d69292f0b97
christy <christy99cc@qq.com>
2019-11-15 02:40:28 +0800

    Update MovieController.java
    增加返回前30电影

--- src/main/java/com/dbcourse/curriculum_design/controller/MoviesController/MovieController.java
+++ src/main/java/com/dbcourse/curriculum_design/controller/MoviesController/MovieController.java
@@ -3,6 +3,7 @@ package com.dbcourse.curriculum_design.controller.MoviesController;
 import com.dbcourse.curriculum_design.controller.MoviesController.been.response.MovieInfoResponse;
 import com.dbcourse.curriculum_design.controller.MoviesController.been.response.ScoreCount;
 import com.dbcourse.curriculum_design.controller.MoviesController.been.response.TagsInfoResponse;
+import com.dbcourse.curriculum_design.controller.MoviesController.been.response.TopNumMovieInfoResponse;
 import com.dbcourse.curriculum_design.model.Movies;
 import com.dbcourse.curriculum_design.model.MoviesAndStaffs;
 import com.dbcourse.curriculum_design.model.ShortComments;
@@ -101,6 +102,18 @@ public class MovieController {
     public TagsInfoResponse MytenTags() {
         List<Tags> tags = tagsService.getTopNumTags(10);
         return new TagsInfoResponse(tags);
+    }
+
+    /**
+     * 返回前30个电影
+     *
+     * @param
+     * @return
+     */
+    @RequestMapping(value = "/movies", method = RequestMethod.GET)
+    public TopNumMovieInfoResponse My30Tags() {
+        List<Movies> movies = moviesService.getTopNumMovies(30);
+        return new TopNumMovieInfoResponse(movies);
     }
 
 }
