edd0386795ccb629434abc4bcffe16796db17af7
Bloomberg2000 <pxfwxhpb@live.com>
2019-11-17 18:26:30 +0800

    Add UsersAndLongCommentsAndMovies

---
+++ src/main/java/com/dbcourse/curriculum_design/model/UsersAndLongCommentsAndMovies.java
@@ -0,0 +1,38 @@
+package com.dbcourse.curriculum_design.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersAndLongCommentsAndMovies implements Serializable {
+    private Integer userid;
+
+    private Integer longcommentsid;
+
+    private Integer movieid;
+
+    private String username;
+
+    private Date createtimedate;
+
+    private Date edittimedate;
+
+    private Short score;
+
+    private String longcommentstitle;
+
+    private String longcommentscontent;
+
+    private String moviecover;
+
+    private String moviename;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/dbcourse/curriculum_design/service/impl/UsersAndLongCommentsAndMoviesServiceImpl.java
@@ -0,0 +1,56 @@
+package com.dbcourse.curriculum_design.service.impl;
+
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import com.dbcourse.curriculum_design.model.UsersAndLongCommentsAndMoviesExample;
+import java.util.List;
+import com.dbcourse.curriculum_design.model.UsersAndLongCommentsAndMovies;
+import com.dbcourse.curriculum_design.mapper.UsersAndLongCommentsAndMoviesMapper;
+import com.dbcourse.curriculum_design.service.UsersAndLongCommentsAndMoviesService;
+@Service
+public class UsersAndLongCommentsAndMoviesServiceImpl implements UsersAndLongCommentsAndMoviesService{
+
+    @Resource
+    private UsersAndLongCommentsAndMoviesMapper usersAndLongCommentsAndMoviesMapper;
+
+    @Override
+    public long countByExample(UsersAndLongCommentsAndMoviesExample example) {
+        return usersAndLongCommentsAndMoviesMapper.countByExample(example);
+    }
+
+    @Override
+    public int deleteByExample(UsersAndLongCommentsAndMoviesExample example) {
+        return usersAndLongCommentsAndMoviesMapper.deleteByExample(example);
+    }
+
+    @Override
+    public int insert(UsersAndLongCommentsAndMovies record) {
+        return usersAndLongCommentsAndMoviesMapper.insert(record);
+    }
+
+    @Override
+    public int insertSelective(UsersAndLongCommentsAndMovies record) {
+        return usersAndLongCommentsAndMoviesMapper.insertSelective(record);
+    }
+
+    @Override
+    public List<UsersAndLongCommentsAndMovies> selectByExample(UsersAndLongCommentsAndMoviesExample example) {
+        return usersAndLongCommentsAndMoviesMapper.selectByExample(example);
+    }
+
+    @Override
+    public int updateByExampleSelective(UsersAndLongCommentsAndMovies record,UsersAndLongCommentsAndMoviesExample example) {
+        return usersAndLongCommentsAndMoviesMapper.updateByExampleSelective(record,example);
+    }
+
+    @Override
+    public int updateByExample(UsersAndLongCommentsAndMovies record,UsersAndLongCommentsAndMoviesExample example) {
+        return usersAndLongCommentsAndMoviesMapper.updateByExample(record,example);
+    }
+
+    @Override
+    public int batchInsert(List<UsersAndLongCommentsAndMovies> list) {
+        return usersAndLongCommentsAndMoviesMapper.batchInsert(list);
+    }
+
+}
