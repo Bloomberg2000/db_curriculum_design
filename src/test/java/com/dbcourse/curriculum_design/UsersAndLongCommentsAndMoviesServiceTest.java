6cdf9625f7a20fc6c8f39e3670a842a2332365d6
christy <christy99cc@qq.com>
2019-11-17 19:54:42 +0800

    用户长评分页测试通过
    本地环境显示没有找到test是为什么

--- src/test/java/com/dbcourse/curriculum_design/UsersAndLongCommentsAndMoviesServiceTest.java
+++ src/test/java/com/dbcourse/curriculum_design/UsersAndLongCommentsAndMoviesServiceTest.java
@@ -15,7 +15,13 @@ public class UsersAndLongCommentsAndMoviesServiceTest {
 
     @Test
     void selectByUserIdTest(){
-        List<UsersAndLongCommentsAndMovies> list = usersAndLongCommentsAndMoviesService.selectByUserId(3, 2, 2);
+        System.out.println("test1:");
+        List<UsersAndLongCommentsAndMovies> list = usersAndLongCommentsAndMoviesService.selectByUserId(3, 1, 2);
+        list.forEach(System.out::println);
+        list.size();//长评总数
+
+        System.out.println("test2:");
+        list = usersAndLongCommentsAndMoviesService.selectByUserId(3, 2, 1);
         list.forEach(System.out::println);
         list.size();//长评总数
     }
