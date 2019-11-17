601618714e3a710b9850356e67bd6d38306a0e3b
christy <christy99cc@qq.com>
2019-11-17 19:22:25 +0800

    Update UsersAndLongCommentsAndMoviesService.java
    用户长评 接口

--- src/main/java/com/dbcourse/curriculum_design/service/UsersAndLongCommentsAndMoviesService.java
+++ src/main/java/com/dbcourse/curriculum_design/service/UsersAndLongCommentsAndMoviesService.java
@@ -22,4 +22,13 @@ public interface UsersAndLongCommentsAndMoviesService{
 
     int batchInsert(List<UsersAndLongCommentsAndMovies> list);
 
+    /**
+     * 显示在用户个人信息页上
+     * 通过用户id找到发表的长评，显示长评总数 + list长评题目/内容///打分/用户名/电影名/电影封面
+     * @author Christy
+     * @param userId 用户id
+     * @return a list of UsersAndLongComments
+     */
+    List<UsersAndLongCommentsAndMovies> selectByUserId(Integer userId);
+
 }
